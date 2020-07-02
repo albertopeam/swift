b81713d89d98a359a6d874d43b396177560967bf
Alberto Penas Amor <alberto.penas@nomasystems.com>
2020-07-02 14:22:18 +0200

    added feature toggles

---
+++ FeatureToggles/FeatureToggles.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+struct ToggleConfiguration {
+    let xFeature: Bool
+}
+
+protocol FeatureToggle {
+    func optional<T: Any>(for path: KeyPath<ToggleConfiguration, Bool>,_ t: T) -> T?
+}
+
+class AppFeatureToggle: FeatureToggle {
+    private let configuration: ToggleConfiguration
+
+    init(configuration: ToggleConfiguration) {
+        self.configuration = configuration
+    }
+
+    func optional<T>(for path: KeyPath<ToggleConfiguration, Bool>, _ t: T) -> T? {
+        if configuration[keyPath: path] {
+            return t
+        }
+        return nil
+    }
+}
+let configuration = ToggleConfiguration(xFeature: false)
+let featureToggle: FeatureToggle = AppFeatureToggle(configuration: configuration)
+
+// returning type
+let property = featureToggle.optional(for: \.xFeature, 1)
+print(property?.advanced(by: 1))
+
+// returning function
+func cool() {
+    print("Cool")
+}
+let function = featureToggle.optional(for: \.xFeature, cool)
+function?()
