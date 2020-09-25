742fe6f494375b7ba6352957953ed1a750d3c3e7
Alberto Penas Amor <alberto__pa@hotmail.com>
2020-09-25 09:58:01 +0200

    create project

---
+++ CombineVsRXSwift/CombineVsRXSwiftTests/CombineVsRXSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  CombineVsRXSwiftTests.swift
+//  CombineVsRXSwiftTests
+//
+//  Created by Alberto Penas Amor on 25/09/2020.
+//  Copyright © 2020 com.github.albertopeam. All rights reserved.
+//
+
+import XCTest
+@testable import CombineVsRXSwift
+
+class CombineVsRXSwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
